5d2d8af6e8c5811377100ac85c76d3a6463c4b06
cleverlf <clf.nameless@gmail.com>
2025-03-17 15:50:26 -0300

    initial commit

---
+++ SistemaDeTarefas/Enums/StatusTarefa.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace SistemaDeTarefas.Enums
+{
+    public enum StatusTarefa
+    {
+        [Description("A Fazer")]
+        AFazer = 1,
+        [Description("Em Andamento")]
+        EmAndamewnto = 2,
+        [Description("Concluido")]
+        Concluido = 3
+         
+    }
+}
